4b0504572a7b0c04c4e6f74fee2d1fe38021d851
kulwamkumba <kulwa003@gmail.com>
2020-07-24 16:51:21 +0300

    cinema-reservation-system-fina-exam-php

---
+++ database/cinema_db.sql
@@ -0,0 +1,78 @@
+
+
+CREATE TABLE IF NOT EXISTS `bookingTable` (
+  `bookingID` int(11) NOT NULL,
+  `movieName` varchar(100) DEFAULT NULL,
+  `bookingTheatre` varchar(100) NOT NULL,
+  `bookingType` varchar(100) DEFAULT NULL,
+  `bookingDate` varchar(50) NOT NULL,
+  `bookingTime` varchar(50) NOT NULL,
+  `bookingFName` varchar(100) NOT NULL,
+  `bookingLName` varchar(100) DEFAULT NULL,
+  `bookingPNumber` varchar(12) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=latin1;
+
+
+CREATE TABLE IF NOT EXISTS `feedbackTable` (
+  `msgID` int(12) NOT NULL,
+  `senderfName` varchar(50) NOT NULL,
+  `senderlName` varchar(50) DEFAULT NULL,
+  `sendereMail` varchar(100) NOT NULL,
+  `senderfeedback` varchar(500) DEFAULT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+
+INSERT INTO `feedbackTable` (`msgID`, `senderfName`, `senderlName`, `sendereMail`, `senderfeedback`) VALUES
+(1, 'Ahmed', 'Ali', 'Ahmed@mail.com', 'Hello first'),
+(2, 'Ahmed', 'Ali', 'asa@as.com', 'asdas');
+
+
+CREATE TABLE IF NOT EXISTS `movieTable` (
+  `movieID` int(11) NOT NULL,
+  `movieImg` varchar(150) NOT NULL,
+  `movieTitle` varchar(100) NOT NULL,
+  `movieGenre` varchar(50) NOT NULL,
+  `movieDuration` int(11) NOT NULL,
+  `movieRelDate` date NOT NULL,
+  `movieDirector` varchar(50) NOT NULL,
+  `movieActors` varchar(150) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
+
+
+INSERT INTO `movieTable` (`movieID`, `movieImg`, `movieTitle`, `movieGenre`, `movieDuration`, `movieRelDate`, `movieDirector`, `movieActors`) VALUES
+(1, 'img/movie-poster-1.jpg', 'Captain Marvel', ' Action, Adventure, Sci-Fi ', 220, '2018-10-18', 'Anna Boden, Ryan Fleck', 'Brie Larson, Samuel L. Jackson, Ben Mendelsohn'),
+(2, 'img/movie-poster-2.jpg', 'Qarmat Bitamrmat  ', 'Comedy', 110, '2018-10-18', 'Assad Fouladkar', 'Ahmed Adam, Bayyumy Fouad, Salah Abdullah , Entsar, Dina Fouad '),
+(3, 'img/movie-poster-3.jpg', 'The Lego Movie', 'Animation, Action, Adventure', 110, '2014-02-07', 'Phil Lord, Christopher Miller', 'Chris Pratt, Will Ferrell, Elizabeth Banks'),
+(4, 'img/movie-poster-4.jpg', 'Nadi Elregal Elserri ', 'Comedy', 105, '2019-01-23', ' Ayman Uttar', 'Karim Abdul Aziz, Ghada Adel, Maged El Kedwany, Nesreen Tafesh, Bayyumy Fouad, Moataz El Tony '),
+(5, 'img/movie-poster-5.jpg', 'VICE', 'Biography, Comedy, Drama', 132, '2018-12-25', 'Adam McKay', 'Christian Bale, Amy Adams, Steve Carell'),
+(6, 'img/movie-poster-6.jpg', 'The Vanishing', 'Crime, Mystery, Thriller', 107, '2019-01-04', 'Kristoffer Nyholm', 'Gerard Butler, Peter Mullan, Connor Swindells');
+
+
+ALTER TABLE `bookingTable`
+  ADD PRIMARY KEY (`bookingID`),
+  ADD UNIQUE KEY `bookingID` (`bookingID`),
+  ADD KEY `bookingID_2` (`bookingID`),
+  ADD KEY `bookingID_3` (`bookingID`),
+  ADD KEY `bookingID_4` (`bookingID`);
+
+
+ALTER TABLE `feedbackTable`
+  ADD PRIMARY KEY (`msgID`),
+  ADD UNIQUE KEY `msgID` (`msgID`);
+
+
+ALTER TABLE `movieTable`
+  ADD PRIMARY KEY (`movieID`),
+  ADD UNIQUE KEY `movieID` (`movieID`);
+
+
+
+ALTER TABLE `bookingTable`
+  MODIFY `bookingID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=23;
+
+ALTER TABLE `feedbackTable`
+  MODIFY `msgID` int(12) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+
+ALTER TABLE `movieTable`
+  MODIFY `movieID` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=12;
+
---
+++ includes/footer.php
@@ -0,0 +1,15 @@
+<div class="footer-section footer-section1">
+    <h2><i class="fas fa-user-alt"></i> Social Media</h2>
+    <div class="footer-section-inner-container">
+        <span><i class="fab fa-lg fa-facebook-square"></i> Facebook</span>
+        <span><i class="fab fa-lg fa-twitter-square"></i> Twitter</span>
+        <span><i class="fab fa-lg fa-instagram"></i> Instagram</span>
+    </div>
+</div>
+
+<div class="footer-section  footer-section3">
+    <p>© 2020 UMUmovies. Created by Mkumba Kulwa Mussa. </p>
+    <a>Advertising</a>
+    <a>Privacy Policy</a>
+    <a>Contact </a>
+</div>
